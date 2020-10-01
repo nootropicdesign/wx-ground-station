c5336f719024d9f43797a1cf4d0a8e2e2d8f5ec0
Michael Krumpus <michael@nootropicdesign.com>
2020-10-01 07:32:19 -0500

    Changed Leaflet tile layer API due to updated MapBox API.

--- website/wx-ground-station.js
+++ website/wx-ground-station.js
@@ -95,12 +95,15 @@ function load() {
       };
       var groundTrackMap = L.map(mapId, mapOptions).setView([GROUND_STATION_LAT, GROUND_STATION_LON], 4);
 
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+        tileSize: 512,
         maxZoom: 18,
-        id: 'mapbox.streets',
+        zoomOffset: -1,
+        id: 'mapbox/streets-v11',
         accessToken: MAP_BOX_ACCESS_TOKEN
       }).addTo(groundTrackMap);
+
       var bounds = groundTrackMap.getBounds();
       var marker = L.marker([GROUND_STATION_LAT, GROUND_STATION_LON], {title: GROUND_STATION_NAME}).addTo(groundTrackMap);
 
